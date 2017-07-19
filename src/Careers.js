6eeb6d1874e92af9b81056f046be70202a1bf0ff
2048209934 <502520081@qq.com>
2017-07-19 09:36:07 +0800

    改

---
+++ src/Careers.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Careers extends Component {
+    componentDidMount = function () {
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="my-careers">
+                    12313
+                </div>
+            </div>
+        )
+    }
+}
+export default Careers
